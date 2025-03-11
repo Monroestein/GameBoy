88e82dac1726181d6f0c51ca6b19edb71318ba9f
Josefina <134737124+Monroestein@users.noreply.github.com>
2025-03-11 14:46:03 -0300

    Build game screen

---
+++ src/Screen.jsx
@@ -0,0 +1,21 @@
+
+//    🌼がんばれ🌼
+
+import React from "react"
+import './Screen.css'
+
+const Screen = () => {
+  return (
+    <>
+      <main>
+        <div className="game-screen">
+          Hello
+        </div>
+      </main>
+      <span className="bottom-screen"></span>
+    </>
+  )
+};
+
+export default Screen;
+
---
+++ src/components/Banner.jsx
@@ -0,0 +1,14 @@
+
+//    🌼がんばれ🌼
+
+import React from "react"
+
+const Banner = () => {
+  return (
+    <div className="bottom-banner">
+      what would -- do
+    </div>
+  )
+};
+
+export default Banner
